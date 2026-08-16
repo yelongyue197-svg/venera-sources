38585ee7c31af6e46d99a8dffe82c47b944bf1e3
yelongyue197-svg <yelongyue197@gmail.com>
2026-08-16 21:00:18 +0800

    Use universal @main urls

--- dm5.js
+++ dm5.js
@@ -5,7 +5,7 @@ class DongManWu extends ComicSource {
   key = "dm5";
   version = "1.0.5";
   minAppVersion = "1.4.0";
-  url = "https://cdn.jsdelivr.net/gh/yelongyue197-svg/venera-sources@v1.0.2/dm5.js";
+  url = "https://cdn.jsdelivr.net/gh/yelongyue197-svg/venera-sources@main/dm5.js";
   api = "https://www.dm5.com";
 
   init() {
